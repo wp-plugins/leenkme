fed952a69d1234ccc7c5cbef8d748fa0f8fe8023
layotte <layotte@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-02 00:48:21 +0000

    = 1.1.7 = * Receiving reports of users with old version of cURL installed, they are not able to verify the SSL (because it is too new). Set the API call to not try to verify the SSL, the SSL still works and encrypts the content though.
    git-svn-id: https://plugins.svn.wordpress.org/leenkme/trunk@306725 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- leenk.me.php
+++ leenk.me.php
@@ -4,12 +4,12 @@ Plugin Name: leenk.me
 Plugin URI: http://leenk.me/
 Description: Automatically publish to your Twitter, Facebook profile/page, and Google Buzz whenever you publish a new post on your WordPress website with the leenk.me social network connector. You need a <a href="http://leenk.me/">leenk.me API key</a> to use this plugin.
 Author: Lew Ayotte @ leenk.me
-Version: 1.1.6
+Version: 1.1.7
 Author URI: http://leenk.me/about/
 Tags: twitter, facebook, google, google buzz, oauth, profile, fan page, image, images, social network, social media, post, posts, twitter post, tinyurl, twitter friendly links, admin, authors, contributors, exclude, category, categories, retweet, republish, rebuzz, connect, status update, leenk.me, leenk me, leenk, scheduled post, smo, social media optimization, ssl, secure
 */
 
-define( 'leenk.me_version' , '1.1.6' );
+define( 'leenk.me_version' , '1.1.7' );
 
 class leenkme {
 	var $options_name			= "leenkme";
@@ -318,7 +318,8 @@ function leenkme_connect( $post ) {
 			$result = $request->request( $dl_pluginleenkme->api_url, 
 											array( 	'method' => 'POST', 
 													'body' => $body, 
-													'headers' => $headers ) );
+													'headers' => $headers,
+													'sslverify' => false ) );
 			
 			if ( isset( $result ) ) {
 				return $result;
@@ -341,7 +342,8 @@ function leenkme_ajax_connect( $connect_arr ) {
 			$result = $request->request( $dl_pluginleenkme->api_url, 
 											array( 	'method' => 'POST', 
 													'body' => $body, 
-													'headers' => $headers ) );
+													'headers' => $headers,
+													'sslverify' => false ) );
 			
 			if ( isset( $result ) ) {
 				return $result;
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: layotte
 Tags: twitter, facebook, google, google buzz, oauth, profile, fan page, image, images, social network, social media, post, posts, twitter post, tinyurl, twitter friendly links, admin, authors, contributors, exclude, category, categories, retweet, republish, rebuzz, connect, status update, leenk.me, leenk me, leenk, scheduled post, smo, social media optimization, ssl, secure
 Requires at least: 2.8
 Tested up to: 3.0.1
-Stable tag: 1.1.6
+Stable tag: 1.1.7
 
 leenk.me empowers you to publish to your Twitter, Facebook, & Google Buzz account whenever you publish a new post in WordPress.
 
@@ -75,6 +75,9 @@ http://leenk.me/2010/09/05/how-to-use-the-leenk-me-google-buzz-plugin-for-wordpr
 Feel free to use the leenk.me [contact form](http://leenk.me/contact) and we will respond as soon as possible.
 
 == Changelog ==
+= 1.1.7 =
+* Receiving reports of users with old version of cURL installed, they are not able to verify the SSL (because it is too new). Set the API call to not try to verify the SSL, the SSL still works and encrypts the content though.
+
 = 1.1.6 =
 * Added SSL to leenk.me API for increased security
 
