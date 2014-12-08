3c388e561d565dddf3a222fd2826cb771d29c119
layotte <layotte@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-08 14:21:13 +0000

    Tagging 2.2.5
    git-svn-id: https://plugins.svn.wordpress.org/leenkme/trunk@1040368 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- leenk.me.php
+++ leenk.me.php
@@ -4,12 +4,12 @@ Plugin Name: leenk.me
 Plugin URI: http://leenk.me/
 Description: Automatically publish to your Twitter, Facebook Profile/Fan Page/Group, and LinkedIn whenever you publish a new post on your WordPress website with the leenk.me social network connector. You need a <a href="http://leenk.me/">leenk.me API key</a> to use this plugin.
 Author: Lew Ayotte @ leenk.me
-Version: 2.2.4
+Version: 2.2.5
 Author URI: http://leenk.me/about/
 Tags: publish, automatic, facebook, twitter, linkedin, friendfeed, fan page, groups, publicize, open graph, social media, social media tools
 */
 
-define( 'LEENKME_VERSION' , '2.2.4' );
+define( 'LEENKME_VERSION' , '2.2.5' );
 
 if ( ! class_exists( 'leenkme' ) ) {
 	
@@ -473,7 +473,6 @@ if ( ! class_exists( 'leenkme' ) ) {
                                     <option value="yourls" <?php selected( 'yourls', $leenkme_settings['url_shortener'] ); ?>>YOURLS</option>
                                     <option value="isgd" <?php selected( 'isgd', $leenkme_settings['url_shortener'] ); ?>>is.gd</option>
                                     <option value="wpme" <?php selected( 'wpme', $leenkme_settings['url_shortener'] ); ?>>wp.me</option>
-                                    <option value="owly" <?php selected( 'owly', $leenkme_settings['url_shortener'] ); ?>>ow.ly</option>
                                     <option value="tinyurl" <?php selected( 'tinyurl', $leenkme_settings['url_shortener'] ); ?>>TinyURL</option>
                                     <option value="tflp" <?php selected( 'tflp', $leenkme_settings['url_shortener'] ); ?>>Twitter Friendly Links Plugin</option>
                                     <option value="wppostid" <?php selected( 'wppostid', $leenkme_settings['url_shortener'] ); ?>>WordPress Post ID</option>
